21688fba04a9e0894a9ebe6365196d6903c95db1
RickBillie-pixel <rickverhoeven0203@gmail.com>
2025-07-22 12:52:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ async def process_pdf(file: UploadFile):
         file_content = await file.read()
         files = {'file': (file.filename, file_content, 'application/pdf')}
         params = {
-            'minify': 'false',  # Use non-minified output
+            'minify': 'true',  # Use non-minified output
             'remove_non_essential': 'false',
             'precision': '2'
         }
